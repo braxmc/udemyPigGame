720b49b7dc87a756860486d3ea8516975446433c
Braxton McClellan <thebraxmc@gmail.com>
2021-02-27 20:55:03 -0700

    init push, started selecting elements

---
+++ script.js
@@ -0,0 +1,10 @@
+'use strict';
+
+// selecting elements
+const score0El = document.getElementById('score--0');
+const score1El = document.getElementById('score--1');
+const diceEl = document.querySelector('.dice');
+
+// starting conditions
+diceEl.classList.add('hidden')
+
