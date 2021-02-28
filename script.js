af24af38409c84f8c18d1b02d7deee5599205aab
Braxton McClellan <thebraxmc@gmail.com>
2021-02-27 22:50:42 -0700

    added new game functions

--- script.js
+++ script.js
@@ -17,6 +17,7 @@ const btnHold = document.querySelector('.btn--hold');
 const scores = [0, 0];
 let currentScore = 0;
 let currentPlayer = 0;
+let playing = true;
 
 function switchPlayer() {
       // swtich to next player, reset score to 0
@@ -32,28 +33,62 @@ diceEl.classList.add('hidden');
 
 // roll the dice
 btnRoll.addEventListener('click', function() {
-  // generate random dice roll
-  const dice = Math.trunc(Math.random() * 6) + 1;
+  if(playing) {
+    // generate random dice roll
+    const dice = Math.trunc(Math.random() * 6) + 1;
 
-  // display dice
-  diceEl.classList.remove('hidden');
-  diceEl.src = `dice-${dice}.png`;
+    // display dice
+    diceEl.classList.remove('hidden');
+    diceEl.src = `dice-${dice}.png`;
 
-  // check for rolling 1
-  if(dice !== 1) {
-  // add current score
-    currentScore += dice;
-    document.getElementById(`current--${currentPlayer}`).textContent = currentScore;
-  } else {
-    switchPlayer();
+    // check for rolling 1
+    if(dice !== 1) {
+    // add current score
+      currentScore += dice;
+      document.getElementById(`current--${currentPlayer}`).textContent = currentScore;
+    } else {
+      switchPlayer();
+    }
   }
-
-  console.log(dice);
 })
 
+// hold button
 btnHold.addEventListener('click', function() {
-  // add current score to players score
-  scores[currentPlayer] += currentScore;
-  document.getElementById(`score--${currentPlayer}`).textContent = scores[currentPlayer];
-  switchPlayer();
+  if(playing) {
+     // add current score to players score
+    scores[currentPlayer] += currentScore;
+    document.getElementById(`score--${currentPlayer}`).textContent = scores[currentPlayer];
+
+    if(scores[currentPlayer] >= 10) {
+      // finishes game
+      playing = false;
+      diceEl.classList.add('hidden');
+      document.querySelector(`.player--${currentPlayer}`).classList.add('player--winner');
+      document.querySelector(`.player--${currentPlayer}`).classList.remove('player--active');
+    } else {
+      switchPlayer();
+    }
+  }
+});
+
+// new game
+btnNew.addEventListener('click', function() {
+  //resets styling
+  document.querySelector(`.player--${currentPlayer}`).classList.remove('player--winner');
+  document.querySelector(`.player--${currentPlayer}`).classList.remove('player--active');
+  diceEl.classList.add('hidden');
+  // resets current score
+  document.getElementById(`current--${currentPlayer}`).textContent = 0;
+  currentScore = 0;
+  // back to player 0
+  currentPlayer = 0;
+  // puts stying back to player 1
+  document.querySelector(`.player--${currentPlayer}`).classList.add('player--active');
+  // sets scores back to 0 current/total
+  document.getElementById(`score--0`).textContent = 0;
+  document.getElementById(`score--1`).textContent = 0;
+  scores[currentPlayer] = 0;
+  scores[!currentPlayer] = 0;
+  // allows you to play again
+  playing = true;
 })
